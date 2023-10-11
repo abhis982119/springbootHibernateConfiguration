3cc9ed805949f760d7c5391e8dbcec5506756b86
Abhishek Sharma <abhisheksharma@Abhisheks-MacBook-Pro.local>
2023-10-11 09:29:25 +0530

    spring-hibernate config with hikari connection pool

---
+++ SpringOrm/src/test/java/com/springOrm/SpringOrm/SpringOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springOrm.SpringOrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
